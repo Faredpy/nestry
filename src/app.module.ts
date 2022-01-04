32ddf714ef3dc48de3015182643d0fe341223401
Faredpy <fareddru@gmail.com>
2022-01-04 20:59:32 +0300

    Pre-auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import {Module} from "@nestjs/common"
 import {SequelizeModule} from "@nestjs/sequelize"
 import {UsersModule} from "./users/users.module";
+import {RolesModule} from './roles/roles.module';
 // import {ConfigModule} from "@nestjs/config";
 import {config} from "dotenv"
 import {User} from "./users/users.model";
+import {Role} from "./roles/roles.model";
+import {UserRoles} from "./roles/user-roles.model";
 config()
 
 @Module({
@@ -18,13 +21,14 @@ config()
             host: process.env.POSTGRES_HOST,
             port: Number(process.env.POSTGRES_PORT),
             username: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
+             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User],
+            models: [User, Role, UserRoles],
             // еще один класс флаг, чтобы секвалайз создавал таблицы в бд на основании тех моделей которые мы будем создавать
             autoLoadModels: true
         }),
         UsersModule,
+        RolesModule,
     ]
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,22 +2,29 @@ import { Injectable } from '@nestjs/common';
 import {User} from "./users.model";
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {RolesService} from "../roles/roles.service";
 
 @Injectable()
 export class UsersService {
 
-    constructor(@InjectModel(User) private userRepository: typeof User) {
+    constructor(@InjectModel(User) private userRepository: typeof User,
+                private roleService: RolesService) {
     }
 
     async createUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto);
-        console.log(user)
-        return user
+        const role = await this.roleService.getRoleByValue("USER")
+        // здесь необходимо указать, что данная роль принадлежит пользователю,
+        // для этого используем метод $set который позволяет перезаписать
+        // какое-либо поле и сразу обновить его внутри базы данных
+        // поскольку изначально у пользователя ролей нет указываем массив
+        // в который добавляем один единственный id
+        await user.$set('roles', [role.id])
+        return user;
     }
 
     async getAllUser() {
-        const users = await this.userRepository.findAll();
-        console.log(users)
+        const users = await this.userRepository.findAll({include: {all: true}});
         return users;
     }
 
